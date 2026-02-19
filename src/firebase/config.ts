88aea3aa8ac6c8a085b4ec4edab76285d773b786
Saurav Karki <sauravk@elietahari.com>
2026-02-19 19:41:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6685988200-7cfc0",
+  "appId": "1:234586101927:web:23389fa1a7fbbd415c2382",
+  "apiKey": "AIzaSyD_uUnlzsY4rQubmRd-jS2lpsUOsd4Tdu0",
+  "authDomain": "studio-6685988200-7cfc0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "234586101927"
+};
