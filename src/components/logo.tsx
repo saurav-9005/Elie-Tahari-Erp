bcdcd50b468bbb4df2566f66d8a7a5bb1814b3a8
Saurav Karki <sauravk@elietahari.com>
2026-02-23 16:12:41 +0000

    check the log.tsx i uploaded teh logo.png inside the components folder

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,14 +2,14 @@
 
 import Image from 'next/image';
 import { cn } from '@/lib/utils';
+import logo from './logo.png';
 
 export function Logo({ className }: { className?: string }) {
   return (
     <Image
-      src="/logo.png"
+      src={logo}
       alt="Elie Tahari"
       width={200}
-      height={40}
       className={cn(className)}
       priority
     />
